463c506508c10095cbf3b04058b3238e3714fc7f
Israel Batista <israel.fbB@gmail.com>
2022-06-03 17:03:29 -0300

    Feedback steps criados e finalizado toda parte web

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,30 +1,32 @@
-import { CloseButton } from '../CloseButton';
-import bugImageUrl from '../../assets/bug.svg';
-import ideaImageUrl from '../../assets/idea.svg';
-import thoughtImageUrl from '../../assets/thought.svg';
-import { useState } from 'react';
-import { FeedbackTypeStep } from './steps/FeedbackTypeStep';
+import { useState } from "react";
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+
+import { FeedbackTypeStep } from "./steps/FeedbackTypeStep";
+import { FeedbackContetStep } from "./steps/FeedbaclContentStep";
+import { FeedbackSucessStep } from "./steps/FeedbackSucessStep";
 
 export const feedbackTypes = {
   BUG: {
-    title: 'Problem',
+    title: "Problema",
     image: {
       source: bugImageUrl,
-      alt: 'Imagem de um inseto'
+      alt: "Imagem de um inseto"
     }
   },
   IDEA: {
-    title: 'Ideia',
+    title: "Ideia",
     image: {
       source: ideaImageUrl,
-      alt: 'Imagem de uma lâmpada'
+      alt: "Imagem de uma lâmpada"
     }
   },
   OTHER: {
-    title: 'Outro',
+    title: "Outro",
     image: {
       source: thoughtImageUrl,
-      alt: 'Imagem de um pensamento'
+      alt: "Imagem de um pensamento"
     }
   }
 };
@@ -33,28 +35,37 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-
-        <CloseButton />
-      </header>
-
-      {!feedbackType ? (
+      {feedbackSent ? (
+        <FeedbackSucessStep
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
+      ) : !feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ) : (
-        <p>maneiro</p>
+        <FeedbackContetStep
+          feedbackType={feedbackType}
+          onFeedbackRestartRequested={handleRestartFeedback}
+          onFeedbackSent={() => setFeedbackSent(true)}
+        />
       )}
 
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ pela
+        Feito com ♥ por{" "}
         <a
           className="underline underline-offset-2"
-          href="https://rockeatseat.com.br"
+          href="https://israelbatista.vercel.app/"
+          target="_blank"
         >
-          Rockeatseat
+          Israel Batista
         </a>
       </footer>
     </div>
